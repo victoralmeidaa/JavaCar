b2abe6b989aeaa6f3d881e42742946f17c5876a7
Victor Almeida <32881893+victoralmeidaa@users.noreply.github.com>
2022-12-08 17:36:16 -0300

    FuncionarioDao

--- src/main/java/utfpr/victor/projetopoo2/modelo/dao/FuncionarioDao.java
+++ src/main/java/utfpr/victor/projetopoo2/modelo/dao/FuncionarioDao.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utfpr.victor.projetopoo2.modelo.dao;
+
+import java.util.List;
+import utfpr.victor.projetopoo2.modelo.vo.Funcionario;
 
 /**
  *
  * @author victo
  */
 public interface FuncionarioDao {
+    void cadastrar (Funcionario funcionario);
     
+    void atualizar (Funcionario funcionario);
+    
+    void excluir(Funcionario funcionario);
+    
+    List<Funcionario> listaTodos();
+    
+    Funcionario listaUm(Long id);
 }
