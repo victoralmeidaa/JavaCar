87bc492e000214e0fd573a01df586a9cc7d9844d
Victor Almeida <32881893+victoralmeidaa@users.noreply.github.com>
2022-12-09 23:44:10 -0300

    Funcionario RN

---
+++ src/main/java/utfpr/victor/projetopoo2/modelo/rn/FuncionarioRN.java
@@ -0,0 +1,44 @@
+package utfpr.victor.projetopoo2.modelo.rn;
+
+import java.util.List;
+import org.hibernate.HibernateException;
+import utfpr.victor.projetopoo2.modelo.dao.FuncionarioDao;
+import utfpr.victor.projetopoo2.modelo.dao.FuncionarioDaoImpl;
+import utfpr.victor.projetopoo2.modelo.vo.Funcionario;
+
+/**
+ *
+ * @author victo
+ */
+public class FuncionarioRN {
+    private FuncionarioDao funcionarioDao;
+    
+    public FuncionarioRN(){
+        funcionarioDao = new FuncionarioDaoImpl();
+    }
+    
+    public void cadastrar(Funcionario funcionario){
+        try {
+            funcionarioDao.cadastrar(funcionario);
+        }catch(HibernateException he){
+            System.out.println(he);
+        }
+    }
+    
+    public void atualizar(Funcionario funcionario){
+        funcionarioDao.atualizar(funcionario);
+    }
+    
+    public void excluir(Funcionario funcionario){
+        funcionarioDao.excluir(funcionario);
+    }
+    
+    public List<Funcionario> listTodos(){
+        return funcionarioDao.listaTodos();
+    }
+    
+    public Funcionario listUm(Long id){
+        return funcionarioDao.listaUm(id);
+    }
+    
+}
