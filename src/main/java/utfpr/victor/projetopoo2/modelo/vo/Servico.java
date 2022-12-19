5ad684e7c071c2ec7fe1457e0893b848b0df874a
Victor Almeida <32881893+victoralmeidaa@users.noreply.github.com>
2022-12-19 03:24:08 -0300

    telasss

--- src/main/java/utfpr/victor/projetopoo2/modelo/vo/Servico.java
+++ src/main/java/utfpr/victor/projetopoo2/modelo/vo/Servico.java
@@ -31,6 +31,10 @@ public class Servico {
     
     private String titulo;
 
+    public Servico() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public String getTitulo() {
         return titulo;
     }
--- src/main/java/utfpr/victor/projetopoo2/visao/Tela_Servico.java
+++ src/main/java/utfpr/victor/projetopoo2/visao/Tela_Servico.java
@@ -160,7 +160,7 @@ public class Tela_Servico extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void bCADASTRARActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCADASTRARActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_bCADASTRARActionPerformed
 
     private void bEXCLUIRActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bEXCLUIRActionPerformed
